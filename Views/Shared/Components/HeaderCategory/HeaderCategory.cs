822903000e831b6b66b736f335b2d074e9115522
hunglq <hunglq.dev@gmail.com>
2024-06-15 17:54:00 +0700

    Update

--- Views/Shared/Components/HeaderCategory/HeaderCategory.cs
+++ Views/Shared/Components/HeaderCategory/HeaderCategory.cs
@@ -5,16 +5,24 @@ using VnStockproxx.Models;
 namespace VnStockproxx {
     public class HeaderCategory : ViewComponent
     {
-        enum HeaderCategoryList { ChungKhoan = 3, BatDongSan = 4, TaiChinh = 5, GiaiTri = 6 };
         private readonly CateRepository _cateRepo;
+        private readonly int ChungKhoan = 0;
+        private readonly int BatDongSan = 0;
+        private readonly int TaiChinh = 0;
+        private readonly int GiaiTri = 0;
 
-        public HeaderCategory(VnStockproxxDbContext context)
+        public HeaderCategory(VnStockproxxDbContext context, IConfiguration configuration)
         {
             this._cateRepo = new CateRepository(context);
+
+            ChungKhoan = Int32.Parse(configuration["IdPost:ChungKhoan"]);
+            BatDongSan = Int32.Parse(configuration["IdPost:BatDongSan"]);
+            TaiChinh = Int32.Parse(configuration["IdPost:TaiChinh"]);
+            GiaiTri = Int32.Parse(configuration["IdPost:GiaiTri"]);
         }
         public async Task<IViewComponentResult> InvokeAsync(VnStockproxxDbContext context)
         {
-            int[] categoryIds = new int[] { (int)HeaderCategoryList.ChungKhoan, (int)HeaderCategoryList.BatDongSan, (int)HeaderCategoryList.TaiChinh, (int)HeaderCategoryList.GiaiTri };
+            int[] categoryIds = new int[] { ChungKhoan, BatDongSan, TaiChinh, GiaiTri };
             var categories = await _cateRepo.GetAll().Where(e => categoryIds.Contains(e.Id)).ToListAsync();
             return View(categories); //Default.cshtml
         }
